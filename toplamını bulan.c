bb57b24252b4c8b2bd93d9f8a9b19c2ddf3f81fb
fatma buse borlu <fatmabuse.120@gmail.com>
2020-10-12 01:24:42 +0300

    Add files via upload

---
+++ toplamını bulan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	
+int toplam,x,y;
+printf ("lütfen bir sayi giriniz:");
+scanf ("%d%d",&x,&y);
+toplam=x+y;
+printf ("toplam deðer %d dir",toplam);
+
+return 0;
+}
+
