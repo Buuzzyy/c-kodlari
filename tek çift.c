49e691f53242a133a3614faadf7c7385090208c3
fatma buse borlu <fatmabuse.120@gmail.com>
2020-10-12 00:42:06 +0300

    Add files via upload

---
+++ tek çift.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	
+	int x;
+	
+	printf ("lutfen x sayisini giriniz:");
+	scanf ("%d",&x);
+	 	
+	 	if(x%2==0){
+	 		printf ("x degeri %d ciftir",x);
+		 }
+		 else
+		 printf("x degeriri %d tektir",x);
+	
+        return 0;
+}
+
