726b627b2259b1e814e7737b7fc43278e6d48415
fatma buse borlu <fatmabuse.120@gmail.com>
2020-10-12 01:30:12 +0300

    Add files via upload

---
+++ 18 yaşından küçükleri siteden atan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int yas;
+	printf("lutfen yasinizi giriniz:");
+	scanf("%d",&yas);
+	
+	if(yas<18){
+		printf("Yasinizdan dolayi siteden atildiniz");
+		
+	}
+	else
+	printf("Siteye hosgeldiniz");
+}
