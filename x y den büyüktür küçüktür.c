deaab6a042bee08efc2de493129a79a6e9a07acf
fatma buse borlu <fatmabuse.120@gmail.com>
2020-10-12 18:33:57 +0300

    Add files via upload

---
+++ x y den büyüktür küçüktür.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	printf("lutfen bir x degeri giriniz:");
+	scanf("%d",&x);
+	printf("lutfen bir y degerini giriniz:");
+	scanf("%d",&y);
+	if(x>y){
+	printf("x y den buyuktur.");
+	}
+	else
+	printf("y x den buyuktur.");
+	return 0;
+}
