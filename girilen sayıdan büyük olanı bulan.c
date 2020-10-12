acc33f51c9eeefc483cd4b3527ae9075ee74b4cc
fatma buse borlu <fatmabuse.120@gmail.com>
2020-10-12 18:42:16 +0300

    Add files via upload

---
+++ girilen sayıdan büyük olanı bulan.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	printf ("lutfen x sayisini giriniz:");
+	scanf("%d",&x);
+	printf ("lutfen y sayisini giriniz:");
+	scanf("%d",&y);
+	
+	if(x>y){
+		printf ("%d",x);
+		
+	}
+	else
+	printf ("%d",y);
+	
+	return 0;
+}
